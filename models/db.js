ba3d0541e8303c114ac73e4ae14ce84ab4af2952
BrsaDev <161748604+BrsaDev@users.noreply.github.com>
2024-09-12 16:31:10 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('startkbank_safetech_webhook', 'root', 'Deus2127*', {
+const sequelize = new Sequelize('startkbank_safetech_webhook', 'user', 'pass', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
